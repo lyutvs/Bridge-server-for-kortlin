409422ef8a7a31112c3eacdb47833c25ee2f329f
lyutvs <lyutvs@gmail.com>
2022-04-28 08:49:45 +0900

    📑 :: DefaultImage

---
+++ src/main/kotlin/com/bridge/bridge_server_kotlin/infrastructure/s3/DefaultImage.kt
@@ -0,0 +1,12 @@
+package com.bridge.bridge_server_kotlin.infrastructure.s3
+
+import lombok.AccessLevel
+import lombok.NoArgsConstructor
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+class DefaultImage {
+
+    companion object {
+        const val USER_PROFILE_IMAGE:String = "'a'"
+    }
+}
