da37454ba04c0a1795a3191124a85849193817bd
lyutvs <lyutvs@gmail.com>
2022-04-28 16:22:16 +0900

    ⚡️ :: 이름 변경

--- src/main/kotlin/com/bridge/bridge_server_kotlin/domain/notice/domain/repository/NoticeAttachmentFileRepository.kt
+++ src/main/kotlin/com/bridge/bridge_server_kotlin/domain/notice/domain/repository/NoticeAttachmentFileRepository.kt
@@ -3,5 +3,5 @@ package com.bridge.bridge_server_kotlin.domain.notice.domain.repository
 import com.bridge.bridge_server_kotlin.domain.notice.domain.NoticeAttachmentFile
 import org.springframework.data.repository.CrudRepository
 
-interface NoticeAttachmentFile: CrudRepository<NoticeAttachmentFile, Long> {
+interface NoticeAttachmentFileRepository: CrudRepository<NoticeAttachmentFile, Long> {
 }
