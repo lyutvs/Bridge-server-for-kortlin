7e79eadd5f373d992eb53ec3549e22c22103e82c
lyutvs <lyutvs@gmail.com>
2022-04-28 16:34:26 +0900

    ⚡️ :: @entity 추가

--- src/main/kotlin/com/bridge/bridge_server_kotlin/domain/notice/domain/NoticeAttachmentFile.kt
+++ src/main/kotlin/com/bridge/bridge_server_kotlin/domain/notice/domain/NoticeAttachmentFile.kt
@@ -1,13 +1,11 @@
 package com.bridge.bridge_server_kotlin.domain.notice.domain
 
 import com.bridge.bridge_server_kotlin.global.entity.BaseTimeIdEntity
-import javax.persistence.FetchType
-import javax.persistence.JoinColumn
-import javax.persistence.ManyToOne
-import javax.persistence.Table
+import javax.persistence.*
 import javax.validation.constraints.NotNull
 
 @Table(name = "tbl_notice_attachment_file")
+@Entity
 class NoticeAttachmentFile(
 
     noticeAttachmentFileUrl: String,
