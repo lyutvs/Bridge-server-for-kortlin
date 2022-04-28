274b43720cb2067933aaef054067e3808dc4658d
lyutvs <lyutvs@gmail.com>
2022-04-29 00:50:30 +0900

    📑 :: Cateogry

---
+++ src/main/kotlin/com/bridge/bridge_server_kotlin/domain/category/domain/Category.kt
@@ -0,0 +1,26 @@
+package com.bridge.bridge_server_kotlin.domain.category.domain
+
+import com.bridge.bridge_server_kotlin.domain.user.domain.User
+import com.bridge.bridge_server_kotlin.global.entity.BaseTimeIdEntity
+import org.hibernate.validator.constraints.Length
+import javax.persistence.*
+import javax.validation.constraints.NotNull
+
+@Table(name = "tbl_category")
+@Entity
+class Category(
+
+    title: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: User
+
+): BaseTimeIdEntity() {
+
+    @NotNull
+    @Length(max = 164)
+    var title = title
+        protected set
+
+}
